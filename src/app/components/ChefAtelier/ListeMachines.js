eba7c5829f558a9ecf5bacc9e0bb2fb3c9350f04
apel <apel@MacBook-Pro-dOz.local>
2018-01-07 03:55:13 +0000

    drag & drop test

--- src/app/components/ChefAtelier/ListeMachines.js
+++ src/app/components/ChefAtelier/ListeMachines.js
@@ -1,49 +1,47 @@
 import React from 'react';
-import  { render } from 'react-dom';
+import { render } from 'react-dom';
 import axios from 'axios';
 
 export class ListeMachines extends React.Component {
-   
- 
-  constructor(props , content)  {
-    super(props);
 
 
-this.state = {
-  machines : []
-             };
-    
+    constructor(props, content) {
+        super(props);
+
+
+        this.state = {
+            machines: []
+        };
+
     }
-//////getMACHINEs
+    //////getMACHINEs
     componentDidMount() {
-    axios.get(`http://localhost:8080/machine/all`)
-      .then( (response) => {
+        axios.get(`http://localhost:8080/machine/all`)
+            .then((response) => {
 
-      this.state.machines = response.data;
-      console.log(this.state.machines);
-      this.setState({ });
-      
-///////Actions in java script
+                this.state.machines = response.data;
+                console.log(this.state.machines);
+                this.setState({});
 
-//const s = document.createElement('script');
-//scriptText = document.createTextNode('function myFunction() {  alert("hii"); } ');
-//script.appendChild(scriptText);
-//document.head.appendChild(script);
-    });    
- }
+                ///////Actions in java script
 
-
-  render() {
+                //const s = document.createElement('script');
+                //scriptText = document.createTextNode('function myFunction() {  alert("hii"); } ');
+                //script.appendChild(scriptText);
+                //document.head.appendChild(script);
+            });
+    }
 
 
-    return (
-      
-      
-      <div class="row"> 
-      <div class="col-md-12">   
-          <div class="panel panel-default">
-              <div class="panel-heading">
-                        <h2> Machines</h2>
+    render() {
+
+        return (
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h2> Machines</h2>
                         </div>
                         <div class="panel-body">
                             <div class="table-responsive">
@@ -54,35 +52,35 @@ this.state = {
                                             <th>Marque </th>
                                             <th> Model</th>
                                             <th> Declaration </th>
-                                          
+
                                         </tr>
                                     </thead>
                                     <tbody>
-                                    { 
-                                        this.state.machines.map((machine,key)=>
-                                        <tr class="odd gradeX" key={machine.idMachine}>
-                                            <td> { machine.code } </td>
-                                            <td>{ machine.marque } </td>
-                                            <td>{ machine.model }</td>
-                                           <td> 
-                                             <a href="#declare" class="btn btn-danger btn-lg" id="PanneBtn" >Panne </a>
-                                            
-                                           </td>
-                                        </tr>
-                                         )
+                                        {
+                                            this.state.machines.map((machine, key) =>
+                                                <tr class="odd gradeX" key={machine.idMachine}>
+                                                    <td> {machine.code} </td>
+                                                    <td>{machine.marque} </td>
+                                                    <td>{machine.model}</td>
+                                                    <td>
+                                                        <a href="#declare" class="btn btn-danger btn-lg" id="PanneBtn" >Panne </a>
+
+                                                    </td>
+                                                </tr>
+                                            )
                                         }
-                                        </tbody>
+                                    </tbody>
                                 </table>
                             </div>
-                            
+
                         </div>
                     </div>
-                    </div>
+                </div>
             </div>
 
-     
-    );
-  }
+
+        );
+    }
 
 
 }
--- src/app/components/IndexDT.js
+++ src/app/components/IndexDT.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import  { render } from 'react-dom';
-import { ListeTechniciens} from './DirecteurT/Technicien';
+import { render } from 'react-dom';
+import { ListeTechniciens } from './DirecteurT/Technicien';
 import { Pannes } from './DirecteurT/Pannes'
+import { Draggable, Droppable } from 'react-drag-and-drop'
+import DragSortableList from 'react-drag-sortable'
 
 
-export default  class IndexDT extends React.Component {
- 
+export default class IndexDT extends React.Component {
 
-  constructor(props)  {
+
+  constructor(props) {
     super(props);
   }
-  render(){
 
-    return(
-       <div>
+  render() {
+    var list = [
+      { content: (<div>test1</div>), classes: ['bigger'] },
+      { content: (<div>test2</div>) },
+      { content: (<div>test3</div>), classes: ['bigger'] },
+      { content: (<div>test4</div>) }
+    ];
+
+    var placeholder = (
+      <div className="placeholderContent">PLACEHOLDER</div>
+    );
+    var onSort = function (sortedList, dropEvent) {
+      console.log("sortedList", sortedList, dropEvent);
+    }
+    return (
+      <div>
         <h3>  indexdt</h3>
-       <ListeTechniciens> </ListeTechniciens>
-       <Pannes> </Pannes>
+        <ListeTechniciens> </ListeTechniciens>
+        <Pannes> </Pannes>
+        <div>
+          <DragSortableList items={list} placeholder={placeholder} onSort={onSort} dropBackTransitionDuration={0.3} type="vertical" />
+          <DragSortableList items={list} onSort={onSort} type="horizontal" />
+          <DragSortableList items={list} placeholder={placeholder} onSort={onSort} type="vertical" />
+        </div>
       </div>
+
+
     );
   }
 
-
+  onDrop(data) {
+    console.log(data)
+    // => banana  
+  }
 }
